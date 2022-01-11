1187b45a37bc971bf555cfff8d0bbc82699c3885
Zachary Huff <zach.huff.386@gmail.com>
2022-01-11 14:59:31 -0500

    Add lock to clipboard

--- clipboard/clipboard.go
+++ clipboard/clipboard.go
@@ -11,21 +11,33 @@ extern char * clipboardGet();
 extern void clipboardSet(char *data);
 */
 import "C"
-import "unsafe"
+
+import (
+	"sync"
+	"unsafe"
+)
+
+var (
+	lock = sync.Mutex{}
+)
 
 func Wait() {
 	C.clipboardWait()
 }
 
 func Get() string {
+	lock.Lock()
 	buffer := C.clipboardGet()
 	data := C.GoString(buffer)
 	C.free(unsafe.Pointer(buffer))
+	lock.Unlock()
 	return data
 }
 
 func Set(data string) {
+	lock.Lock()
 	buffer := C.CString(data)
 	C.clipboardSet(buffer)
 	C.free(unsafe.Pointer(buffer))
+	lock.Unlock()
 }
